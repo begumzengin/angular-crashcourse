61b67e681066c5d0e7c9399ef60efa7405d5c8c1
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2023-07-19 15:26:32 +0300

    set up json-server and fetched data from server

--- angular-crash/src/app/services/task.service.ts
+++ angular-crash/src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import {TASKS} from '../mock-tasks';
 import { Task } from 'src/app/Task';
@@ -7,11 +8,11 @@ import { Task } from 'src/app/Task';
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
