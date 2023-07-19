170a6bf3594408e7f9c6b1ef1761f75047a5c704
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2023-07-19 17:03:10 +0300

    addTask functionality implemented

--- angular-crash/src/app/app.module.ts
+++ angular-crash/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 //components
 import { AppComponent } from './app.component';
@@ -25,7 +26,8 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
